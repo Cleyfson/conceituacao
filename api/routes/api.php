1f4b861fa57ba4c5d4311b8d848cfc70eddb5870
cleifson <cl.raujo1@gmail.com>
2025-06-13 11:47:44 -0300

    fix(api): adjust not-admin access

--- api/routes/api.php
+++ api/routes/api.php
@@ -22,14 +22,18 @@ Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
 });
 
-
 Route::middleware('auth:api')->group(function () {
     Route::post('auth/logout', [AuthController::class, 'logout']);
-    
-    Route::apiResource('users', UserController::class);
+
+    Route::get('users', [UserController::class, 'index']);
+    Route::get('users/{user}', [UserController::class, 'show']);
     Route::get('users/{user}/profiles', [UserController::class, 'getProfiles']);
 
     Route::middleware('is.admin')->group(function () {
+        Route::post('users', [UserController::class, 'store']);
+        Route::put('users/{user}', [UserController::class, 'update']);
+        Route::delete('users/{user}', [UserController::class, 'destroy']);
+
         Route::apiResource('profiles', ProfileController::class);
 
         Route::prefix('users/{user}')->group(function () {
